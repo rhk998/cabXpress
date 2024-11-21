c87f55344422a34c398b657406449532819afc21
rhk998 <hemanthk.998r@gmail.com>
2024-11-21 07:42:50 +0530

    first commmit

---
+++ kunal/cabXpress/src/test/java/com/Travels/cabXpress/CabXpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Travels.cabXpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabXpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
