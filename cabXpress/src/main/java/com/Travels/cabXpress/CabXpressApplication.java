3440172e4f3586386096c5e1f0d6296b2a69ed2b
rhk998 <hemanthk.998r@gmail.com>
2024-11-21 07:51:40 +0530

    init commmit

---
+++ cabXpress/src/main/java/com/Travels/cabXpress/CabXpressApplication.java
@@ -0,0 +1,13 @@
+package com.Travels.cabXpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabXpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabXpressApplication.class, args);
+	}
+
+}
